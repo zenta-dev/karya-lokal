56253a9c40aa9d5ce3b99c945392dc8cecc7b0b8
Turbobot <turbobot@vercel.com>
2023-10-07 13:21:12 +0700

    feat(create-turbo): apply official-starter transform

--- apps/cms/src/app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-type ResponseData = {
-  message: string;
-};
-
-export async function GET() {
-  return NextResponse.json<ResponseData>({ message: "Hello World!" });
-}
