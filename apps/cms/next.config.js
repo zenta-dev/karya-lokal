a75549c747b1a13840dc671ba5baaad8423e7b00
runner95up <bokirsianpar95@gmail.com>
2023-10-16 22:23:45 +0700

    chore:fix package.json

--- apps/cms/next.config.js
+++ apps/cms/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
   },
 };
 
