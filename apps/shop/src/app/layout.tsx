64a4b918623b98ab42fc341c597cf98f6af61cdf
violia ruana <violiaruana@gmail.com>
2023-10-20 23:13:49 +0700

    make logo, header, and navbar

--- apps/shop/src/app/layout.tsx
+++ apps/shop/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import { Poppins } from 'next/font/google'
+import Header from './components/Header'
+import NavBar from './components/NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'] ,weight:["400","500","600","700"]}
+)
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header/>
+        <NavBar/>
+        {children}
+      </body>
     </html>
   )
-}
+} 
