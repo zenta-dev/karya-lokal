41778df4791ea359dcb91cc9b5b9da0c1ba21c70
runner95up <bokirsianpar95@gmail.com>
2023-10-26 10:10:22 +0700

    feat:shop

--- apps/cms/src/helpers/get-stock-count.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@karya-lokal/database";
-
-export const getStockCount = async (userId: string) => {
-  const stockCount = await prisma.storeProduct.count({
-    where: {},
-  });
-
-  return stockCount;
-};
