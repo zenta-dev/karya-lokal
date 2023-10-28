964c7f85c7a3e6860a87d5ba636e2d08c3c2f093
runner95up <bokirsianpar95@gmail.com>
2023-10-28 19:17:44 +0700

    chore:fix form body

--- apps/cms/lib/utils.ts
+++ apps/cms/lib/utils.ts
@@ -1,11 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const formatter = new Intl.NumberFormat('en-US', {
+export const formatter = new Intl.NumberFormat('id-ID', {
   style: 'currency',
-  currency: 'USD',
+  currency: 'IDR',
 });
